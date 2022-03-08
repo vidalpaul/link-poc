4470535d2b187c932f963ed820efd5548e26769b
vidalpaul <u1d4lp@gmail.com>
2022-03-08 15:23:05 -0300

    add pks

--- index.js
+++ index.js
@@ -115,6 +115,76 @@ async function startServer() {
    );
 }
 
-async function transfer(encryptedObj, to, amount) {}
+async function transfer(symbol, from, pk, to, amount, gasPrice = null) {
+   const token = 'LINK';
+   console.log(
+      `TokenTX.send(symbol: ${symbol}, to: ${to}, amount: ${amount}, gasPrice: ${gasPrice}, isMint: ${isMint})`
+   );
+   if (!token) {
+      throw new Error('UNRECOGNIZED_TOKEN_ADDRESS');
+   }
+
+   console.log(`-> from: ${from}`);
+
+   const functionSig = token.methods.transfer(to, amount).encodeABI();
+
+   console.log(`-> functionSig: ${functionSig}`);
+
+   const estimatedGas = await token.methods
+      .transfer(to, amount)
+      .estimateGas({ from, data: functionSig })
+      .catch((e) => {
+         console.log('tx', 'FAILED GAS ESTIMATION');
+         throw e;
+      });
+
+   //    gasPrice = await this.resolveGas(gasPrice).catch((e) => {
+   //       throw e;
+   //    });
+
+   //    const nonce = await this.resolveNonce(from).catch((e) => {
+   //       BitfyNonceManager.resetNonce();
+   //       throw e;
+   //    });
+
+   let config = {
+      from,
+      to,
+      gas: estimatedGas,
+      gasPrice,
+      nonce,
+      data: functionSig,
+   };
+
+   console.log(`-> tx: ${JSON.stringify(config, null, 2)}`);
+
+   const pk = 0;
+
+   const signedTx = await this.core.web3.eth.accounts
+      .signTransaction(config, pk)
+      .catch(async (e) => {
+         const atLeast = e.message.split('least ')[1];
+         config.gas = atLeast;
+
+         console.log(`-> Gas too low. Adjusting to ${atLeast}`);
+
+         return await web3.eth.accounts
+            .signTransaction(config, pk)
+            .catch((e) => {
+               BitfyNonceManager.unmute();
+               throw e;
+            });
+      });
+
+   const hash = await this.promise
+      .sendSignedTransaction(config, signedTx.rawTransaction)
+      .catch((e) => {
+         throw e;
+      });
+
+   console.log(`[token] tx ${nonce} sent: ${hash}`);
+
+   return hash;
+}
 
 startServer();
